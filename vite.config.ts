a2f710deec2afc7e033e1952ab74ed9b57073124
Rikovald <rikonvald@gmail.com>
2025-04-22 18:33:33 +0300

    test agfafaa

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
+// import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
-    alias: {
-      //! Иерархия зависемостей, импорты разрешены из директории выше в директории ниже
-      'app': path.resolve('src/app'),
-      'processes': path.resolve('src/processes'),
-      'pages': path.resolve('src/pages'),
-      'widgets': path.resolve('src/widgets'),
-      'features': path.resolve('src/features'),
-      'entities': path.resolve('src/entities'),
-      'shared': path.resolve('src/shared'),
-    },
+    // alias: {
+    //   //! Иерархия зависемостей, импорты разрешены из директории выше в директории ниже
+    //   'app': path.resolve('src/app'),
+    //   'processes': path.resolve('src/processes'),
+    //   'pages': path.resolve('src/pages'),
+    //   'widgets': path.resolve('src/widgets'),
+    //   'features': path.resolve('src/features'),
+    //   'entities': path.resolve('src/entities'),
+    //   'shared': path.resolve('src/shared'),
+    // },
     extensions: ['.ts', '.tsx', '.js'],
   }
 });
