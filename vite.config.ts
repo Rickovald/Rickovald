d9513fa81ff6bbca8f875592686886150abd4192
Rikovald <rikonvald@gmail.com>
2025-04-22 17:47:07 +0300

    ci should work now

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  // resolve: {
-  //   alias: {
-  //     //! Иерархия зависемостей, импорты разрешены из директории выше в директории ниже
-  //     'app': path.resolve(__dirname, 'src/app'),
-  //     'processes': path.resolve(__dirname, 'src/processes'),
-  //     'pages': path.resolve(__dirname, 'src/pages'),
-  //     'widgets': path.resolve(__dirname, 'src/widgets'),
-  //     'features': path.resolve(__dirname, 'src/features'),
-  //     'entities': path.resolve(__dirname, 'src/entities'),
-  //     'shared': path.resolve(__dirname, 'src/shared'),
-  //   }
-  // }
+  resolve: {
+    alias: {
+      //! Иерархия зависемостей, импорты разрешены из директории выше в директории ниже
+      'app': path.resolve(__dirname, 'src/app'),
+      'processes': path.resolve(__dirname, 'src/processes'),
+      'pages': path.resolve(__dirname, 'src/pages'),
+      'widgets': path.resolve(__dirname, 'src/widgets'),
+      'features': path.resolve(__dirname, 'src/features'),
+      'entities': path.resolve(__dirname, 'src/entities'),
+      'shared': path.resolve(__dirname, 'src/shared'),
+    }
+  }
 });
